b78103edfcdeda0ff04f90df745638d25374c20c
Marco Bungart <marco.bungart@googlemail.com>
2023-06-23 14:57:28 +0200

    Make UPX compression configurable

--- scripts/build-compressed.sh
+++ scripts/build-compressed.sh
@@ -4,34 +4,13 @@ set -e
 cd "$(dirname "$(realpath -s "$0")")" || exit 1
 source build-commons.sh
 
-function build_go() {
-  echo "========================================"
-  echo "Getting dependencies"
-  go clean
-  go get \
-    -d `# only get dependencies, do not install them` \
-    -t `# get test dependencies` \
-    -v `# print the name of the packages`
-  echo "----------------------------------------"
-  echo "Running tests"
-  go test
-  echo "----------------------------------------"
-  echo "Building application"
-  go build \
-    -ldflags="-s -w" `# omit symbol table (-s) and DWARF symbol table (-w)` \
-    -o app \
-    -tags netgo
-  echo "----------------------------------------"
-  echo "Compressing application"
-  upx --brute app
-  echo "========================================"
-}
-
 function build() {
   cd ..
   BUILD_CONTAINER="yes"
   build_go
+  compress
   build_container "Containerfile"
+  echo "========================================"
 }
 
 build "${@}"
